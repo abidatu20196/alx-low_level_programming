20d770ef8b5e988f279425a5d310096b67761377
abidatu20196 <tahiduabida@gmail.com>
2022-09-11 18:28:56 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char fun;
+
+	for (fun = 'a'; fun <= 'z'; fun++)
+		putchar(fun);
+	for (fun = 'A'; fun <= 'Z'; fun++)
+		putchar(fun);
+	putchar('\n');
+	return (0);
+}
