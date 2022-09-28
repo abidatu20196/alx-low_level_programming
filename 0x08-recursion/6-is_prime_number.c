4acea9b4c2d935b197e1d3e0b78ba2c31006d7e9
abidatu20196 <tahiduabida@gmail.com>
2022-09-28 16:12:50 -0700

    Inception. Is it possible

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,12 +8,14 @@
  */
 int is_prime(int f, int p)
 {
-	if (p < 2 || p % f == 0)
-		return (0);
-	else if (f > p / 2)
-		return (1);
-	else
-		return (prime_check(f + 1, p));
+if (f == 1)
+	return (0);
+else if (f % p == 0 && p != f && p != 1)
+	return (0);
+else if (f % p == 0 && p == f)
+	return (1);
+else
+	return (is_prime(f, p + 1));
 }
 
 /**
